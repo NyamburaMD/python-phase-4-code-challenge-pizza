a26741b5cf026b0b9d9099d9cf413fe3e69d59f3
darlenenyambura <darlene.nyambura@student.moringaschool.com>
2025-06-06 17:27:54 +0300

    restaurant relationship

--- server/models.py
+++ server/models.py
@@ -21,6 +21,7 @@ class Restaurant(db.Model, SerializerMixin):
     address = db.Column(db.String)
 
     # add relationship
+    restaurant_pizzas = db.relationship("RestaurantPizza", back_populates="restaurant", cascade="all, delete-orphan")
 
     # add serialization rules
 
